45337b2a4787f218cc5c8f1a1c544f43233ed69f
Thequadehunter <Thequadehunter@gmail.com>
2017-08-23 00:44:16 -1000

    cursor offfset is wrong
    cursor offfset is wrong

---
+++ triggerbot/triggerbot/Main.cpp
@@ -0,0 +1,150 @@
+#include <Windows.h>  
+#include <iostream> 
+#include "HookingTool.h" 
+
+CHackProcess fProcess;
+using namespace std;
+
+#define LEFT_CLICK 0x01
+
+/*
+m_iHealth = 0x90
+
+m_iTeam = 0x980  - 8 bytes in front of health
+
+Base Address: "client.dll"+004C6708
+
+m_flags = 0x350
+
+
+Enemy Base: "client.dll"+004D3904
+
+Enemy distance = 0x10
+
+crosshair id offset = 0x145C
+
+NUM OF PLAYERS: server.dll+50F864
+*/
+const DWORD dw_playerBase = 0x004C6708; //client
+const DWORD dw_enemyBase = 0x004D3904; //client
+const DWORD dw_teamOffset = 0x98; //client
+//const DWORD dw_crosshairIdOffset = 0x145C; //from player base - this gets another interesting value
+const DWORD dw_crosshairIdOffset = 0x14D4; //from player base
+const DWORD dw_enemyEntityOffset = 0x10; //client
+const DWORD dw_playerCount = 0X5EF83C; //ENGINE
+
+int getPlayerCount()
+{
+	int players = 0;
+
+	ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)( fProcess.__dwordEngine + dw_playerCount), &players, sizeof(int), 0);
+	return players;
+}
+
+void LeftClick()
+{
+	INPUT    Input = { 0 };
+	// left down 
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+	::SendInput(1, &Input, sizeof(INPUT));
+
+	// left up
+	::ZeroMemory(&Input, sizeof(INPUT));
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+	::SendInput(1, &Input, sizeof(INPUT));
+}
+
+int GetWindowString(HWND hwnd, string &s)
+{
+	char buffer[65536];
+
+	int txtlen = GetWindowTextLength(hwnd) + 1; //idk why this works like this it just does
+	GetWindowText(hwnd, buffer, txtlen); //read window text into char buffer
+
+	s = buffer;
+	return txtlen;
+}
+
+string GetWindowTitle(HWND window)
+{
+	string s;
+	int len = GetWindowString(window, s); //read window name into s
+	return s;
+}
+
+struct MyPlayer_t
+{
+	DWORD localPlayer;
+	int cursorPosition;
+	int team;
+
+	void ReadInformation()
+	{
+		//read player's team
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(fProcess.__dwordClient + dw_playerBase), &localPlayer, sizeof(DWORD), 0);
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(localPlayer + dw_teamOffset), &team, sizeof(int), 0);
+
+		//read cursor position
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(localPlayer + dw_crosshairIdOffset), &cursorPosition, sizeof(int), 0);		
+		cout << cursorPosition << endl;
+	}
+}MyPlayer;
+
+struct EnemyPlayerList_t
+{
+	DWORD localPlayer;
+	int team;
+
+	void ReadInformation(int player)
+	{
+		DWORD playerIndex = player * dw_enemyEntityOffset;
+
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(fProcess.__dwordClient + dw_crosshairIdOffset + playerIndex), &localPlayer, sizeof(DWORD), 0);
+		ReadProcessMemory(fProcess.__HandleProcess, (PBYTE*)(localPlayer + dw_teamOffset), &team, sizeof(int), 0);
+	}
+
+}EnemyPlayerList[32];
+
+void triggerBot()
+{
+	HWND hwnd = GetForegroundWindow();
+	bool enemyInSight = false;
+	int playerCount = getPlayerCount();
+
+	if (MyPlayer.cursorPosition == 0)
+		return;
+	//else if (MyPlayer.cursorPosition > playerCount)
+		//return;
+	//else if (EnemyPlayerList[MyPlayer.cursorPosition - 1].team == MyPlayer.team)
+		//return;
+	else
+	{
+		if (GetWindowTitle(hwnd).find("Counter-Strike") != string::npos)
+			LeftClick();
+	}
+}
+
+
+int main()
+{
+	cout << "test" << endl;
+	fProcess.RunProcess();
+	cout << "CS Source found" << endl;
+
+	int playerCount = getPlayerCount();
+
+	for (;;)
+	{
+		MyPlayer.ReadInformation();
+
+		for (int i = 0; i < playerCount; i++)
+		{
+			EnemyPlayerList[i].ReadInformation(i);
+		}
+		triggerBot();
+	}
+
+	return 0;
+}
